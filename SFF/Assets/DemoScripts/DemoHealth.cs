a21fb29db016680626252e5ec4f6f0d2c2b27b59
Thsng <thsng2k5vn@gmail.com>
2026-03-15 17:59:22 +0700

    Cập nhật Demo part 7

--- SFF/Assets/DemoScripts/DemoHealth.cs
+++ SFF/Assets/DemoScripts/DemoHealth.cs
@@ -4,10 +4,15 @@ public class Health : MonoBehaviour
 {
     public GameObject explosionPrefab;
     public int defaultHealthPoint;
-    private int healthPoint;
+    public int healthPoint;
     public System.Action onDead;
+    public System.Action onHealthChanged;
 
-    private void Start() => healthPoint = defaultHealthPoint;
+    private void Start()
+    {
+        healthPoint = defaultHealthPoint;
+        onHealthChanged?.Invoke();
+    }
 
     protected virtual void Die()
     {
@@ -21,6 +26,7 @@ public class Health : MonoBehaviour
         if (healthPoint <= 0) return;
 
         healthPoint -= damage;
+        onHealthChanged?.Invoke();
         if (healthPoint <= 0) Die();
     }
 }
